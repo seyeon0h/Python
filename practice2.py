880a9b4115f9c617ec65f4f0924559895945cba9
comehereplz <oseyeon101@gmail.com>
2022-06-24 23:42:00 +0900

    practice 2

---
+++ practice2.py
@@ -0,0 +1,31 @@
+# 문자열
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ" * 9)
+
+# boolean 자료형
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+# 변수
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "에요")
+print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+# Quiz) 변수를 이용하여 다음 문장을 출력하시오
+
+station = "인천공항"
+
+print(station + "행 열차가 들어오고 있습니다.")
